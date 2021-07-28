caeb54bcc988a39d97ed48259b355b01cc7b6635
Taufik Mahbub <77428168+taufik9890@users.noreply.github.com>
2021-07-28 20:25:46 +0600

    djk

---
+++ js/active.js
@@ -0,0 +1,29 @@
+(function($) {
+    'use strict';
+
+    $('.adobe_p').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#cfaa9b'
+    });
+    $('.adobe_i').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#cfaa9b'
+    });
+
+
+
+    $('.adobe_x').rProgressbar({
+        percentage: 85,
+        fillBackgroundColor: '#cfaa9b'
+    });
+
+
+
+    $('.sketch').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#cfaa9b'
+    });
+
+
+
+})(jQuery);
---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    'use strict';
+
+
+    // Venobox Start js
+    $('.venobox').venobox();
+
+    // Counter up js
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+
+
+})
